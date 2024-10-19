154c0a011ab4e953f69b968efc97883203647970
lol70000 <tobiluchsinger@bluewin.ch>
2024-10-19 11:45:12 +0200

    added all classes and added theyr initiaalisation functions and got all values from the html document

--- js/index.js
+++ js/index.js
@@ -1,23 +1,48 @@
+document.getElementById("submit").onclick = main;
+
 class team {
     constructor(teamname){
         this.name = String(teamname);
         this.game_times = [];
-    }
+    };
 
     return_name(){
         return String(this.name);
-    }
-}
+    };
+};
 
 class games {
     constructor(team1, team2){
         this.dict = {};
         str1_team = team1.return_name;
         str2_team = team2.return_name;
-        game_string = str1_team + str2_team
-        this.dict.game = String(game_string)
-        this.dict.team1 = str1_team
-        this.dict.team2 = str2_team
+        game_string = str1_team + str2_team;
+        this.dict.game = String(game_string);
+        this.dict.team1 = str1_team;
+        this.dict.team2 = str2_team;
+    };
+};
+
+class game_times {
+    constructor(time_id, num_places){
+        this.time_identifier = time_id;
+        this.places = [];
+    };
+};
+
+class table {
+    constructor(num_game_times) {
+        this.tale_dict = {};
+    };
+};
+
+function main(){
+    num_of_games = document.getElementById("number_of_teams").value;
+    num_of_gyms = document.getElementById("number_of_gyms").value;
+    start_times = document.getElementById("start_time").value;
+    time_per_game = document.getElementById("time_per_game").value;
+    time_per_break = document.getElementById("time_per_break").value;
+    for(i = 0; i < num_of_games; i++){
+        
     }
 }
-
--- readme.md
+++ readme.md
@@ -15,4 +15,4 @@
     - function wich can get random games from games class and check if team not already given for that time wich also checks with the team class to give the team class the game time ans how many games in a row there already are.
 
 - Table
-    - a dict which combines all the gametimes in to one table ans ads the time as the key and then the gametimes as a dict within the dict.
+    - a dict which combines all the gametimes in to one table ans ads the time as the key and then the gametimes as a dict within the dict.1
