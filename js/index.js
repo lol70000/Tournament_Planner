2ffbeea6623a1322e93c9401b6261bb24cfa999a
lol70000 <tobiluchsinger@bluewin.ch>
2025-01-27 16:30:12 +0100

    started with new concept

--- js/index.js
+++ js/index.js
@@ -1,50 +1,22 @@
-//document.getElementById("submit").onclick = main;
+document.getElementById("submit").onclick = main;
 
-class team {
-    constructor(teamname){
-        this.name = String(teamname);
-        this.game_times = [];
-    };
+async function main() {
+    teams = {};
+    games = {};
 
-    return_name(){
-        return String(this.name);
-    };
-};
+    // Get all the vairables from the form
 
-class games {
-    constructor(team1, team2){
-        this.dict = {};
-        str1_team = team1.return_name;
-        str2_team = team2.return_name;
-        game_string = str1_team + str2_team;
-        this.dict.game = String(game_string);
-        this.dict.team1 = str1_team;
-        this.dict.team2 = str2_team;
-    };
-};
+    num_teams = document.getElementById("number_of_teams").value;
+    num_gyms = document.getElementById("number_of_gyms").value;
+    game_time = document.getElementById("time_per_game").value;
+    break_time = document.getElementById("time_per_break").value;
+    start_time = document.getElementById("start_time").value;
 
-class game_times {
-    constructor(time_id, num_places){
-        this.time_identifier = time_id;
-        this.places = [];
-    };
-};
+    // fill the teams table with elemenets for all the teams
 
-class table {
-    constructor(num_game_times) {
-        this.tale_dict = {};
-    };
-};
-
-function main(){
-    num_of_games = document.getElementById("number_of_teams").value;
-    num_of_gyms = document.getElementById("number_of_gyms").value;
-    start_times = document.getElementById("start_time").value;
-    time_per_game = document.getElementById("time_per_game").value;
-    time_per_break = document.getElementById("time_per_break").value;
-    for(i = 0; i < num_of_games; i++){
-        console.log(i);
+    for (i = 1; i <= num_teams; i++) {
+        teams[i] = {num_follow: 0};
     }
-}
-
-main
+    
+    
+};
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Tournament Plan Generator
 
-## class structure
+## class structure(old)
 
 - Team
     - Name (f.E. Team 1/ Team A)
@@ -16,3 +16,28 @@
 
 - Table
     - a dict which combines all the gametimes in to one table ans ads the time as the key and then the gametimes as a dict within the dict.1
+
+## Using a dict
+
+### Proposed structure of Teams Dictionary
+
+````
+dict = {1: {
+    num_follow : 0
+    }
+}
+````
+
+### Proposed structure of Games Dictionary
+
+````
+dict = {1: {
+        game_1: [team,team],
+        ...
+    },
+    {
+        game_1: [team,team],
+        ...
+    }
+}
+````
