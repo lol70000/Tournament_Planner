66417e848adf7cce1b62e96dbeadb5dbb7e0b4da
lol70000 <tobiluchsinger@bluewin.ch>
2025-05-11 16:06:51 +0200

    made the checking function work

--- comp/computing.py
+++ comp/computing.py
@@ -6,35 +6,46 @@ def make_table(teams: int, gyms: int, starttime: str, time_per_game: int, time_p
     number_of_games = len(games_list)
     gym_dict = make_gym_dict(gyms)
 
-    print(team_dict)
-    print("\n")
-    print(games_list)
-    print("\n")
-    print(gym_dict)
-    print("\n")
+    print(number_of_games)
     fill_gyms_table(games_list, gym_dict, team_dict)
-    print(gym_dict)
+    for i in gym_dict:
+        print(gym_dict[i])
     print(team_dict)
 
 def fill_gyms_table(games_list: list, gyms_dict: dict, teams_dict: dict):
+    counter = 0
+    while len(games_list) != 0:
+        for i in gyms_dict:
+            index = int(i)
+            random_index = random.randint(0,len(games_list)-1)
+            selected_game = games_list[random_index]
+            while check_already_if_in_gyms(selected_game, gyms_dict, counter):
+                random_index = random.randint(0,len(games_list)-1)
+                selected_game = games_list[random_index]
+            else:
+                games_list.pop(random_index)
+                gyms_dict[i].append(selected_game)
+                for y in selected_game:
+                    teams_dict[y]["gametimes"].append(index)
+                    teams_dict[y]["number_of_follow_up_games"] += 1
+        counter += 1
+
+
+def check_already_if_in_gyms(selected_game: list, gyms_dict: dict, counter: int) -> bool:
+    check = 0
     for i in gyms_dict:
-        index = int(i)
-        selected_game: list = select_random_game(games_list)
-        status: bool = check_already_if_in_gyms(selected_game, gyms_dict)
-        if not status:
-            gyms_dict[i].append(selected_game)
-            for y in selected_game:
-                teams_dict[y]["gametimes"].append(index)
-                teams_dict[y]["number_of_follow_up_games"] += 1
+        if len(gyms_dict[i]) <= counter:
+            continue
+        for selected_team in selected_game:
+            if selected_team in gyms_dict[i][counter]:
+                check+=1
+    print(check)
+    if check > 0:
+        return True
+    else:
+        return False
 
-
-def check_already_if_in_gyms(selected_game: list, gyms_dict: dict) -> bool:
-    for i in gyms_dict:
-        if selected_game in gyms_dict[i]:
-            return True
-    return False
-
-def select_random_game(games: list) -> list:
+def select_random_game(games: list,) -> list:
     return games.pop(random.randint(0,len(games)-1))
 
 def make_gym_dict(gyms: int) -> dict:
